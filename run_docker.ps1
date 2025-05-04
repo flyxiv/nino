1d22be95ea2fa2ac438348fed28a88fd9f55228f
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-05-04 15:10:09 +0900

    added

--- run_docker.ps1
+++ run_docker.ps1
@@ -3,6 +3,6 @@ $image_name = $args[0]
 # Docker 실행 명령
 docker run --gpus all -it `
   -v ${PWD}/data/instance_segmentation_coco:/nino/data `
-  -v ${PWD}/nino_seg_maskrcnn_e500.pth:/nino/nino_seg_maskrcnn_e500.pth `
+  -v ${PWD}/trained_models:/nino/trained_models `
   -v ${PWD}/output_data:/nino/output_data `
   "$image_name" bash
