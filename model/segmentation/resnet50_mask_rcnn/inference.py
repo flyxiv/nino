cd8510f7ea3c2e138d7231cc3ed31ff224fffebf
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-05-04 14:25:32 +0900

    added inference script

--- model/segmentation/resnet50_mask_rcnn/inference.py
+++ model/segmentation/resnet50_mask_rcnn/inference.py
@@ -1,37 +1,30 @@
-"""Inference ResNet50 Mask R-CNN model.
+from mmdet.apis import init_detector, inference_detector
+import numpy as np
 
-Model is trained from mmdetection library.
-"""
+def create_from_mask_rcnn_output(mask, bbox, label, score):
+    mask = mask.astype(np.float32) 
 
-def create_from_yolo_output(mask, bbox):
     return SegmentedModelOutput(
         bbox_mask=mask,
         bbox_x1=bbox[0],
         bbox_y1=bbox[1],
         bbox_x2=bbox[2],
         bbox_y2=bbox[3],
-        bbox_conf=bbox[4],
-        bbox_class=bbox[5]
+        bbox_conf=score,
+        bbox_class=label
     )
 
 def create_segmented_model_outputs_mask_rcnn(img_batch, model, conf_threshold: float = 0.9):
     """Collect model outputs and their corresponding images from a single image or a batch of images.
     """
-    results = model(img_batch, device=0)  
+    batch_results = inference_detector(img_batch, device=0)  
     model_outputs = []
 
-    for (result_idx, result) in enumerate(results):
-        if result.masks is not None and result.boxes is not None:
-            masks = result.masks.data.cpu().numpy()  
-            boxes = result.boxes.data.cpu().numpy()  
-            
-            for mask, box in zip(masks, boxes):
-                model_output = create_from_yolo_output(mask, box)
-                if model_output.bbox_conf > conf_threshold:
-                    if type(img_batch) == list:
-                        img = img_batch[result_idx]
-                    else:
-                        img = img_batch
-                    model_outputs.append((model_output, img))
+    for result_idx, result in enumerate(batch_results):
+        img = img_batch[result_idx]
+
+        for bbox, label, score, mask in zip(result.pred_instances.bboxes, result.pred_instances.labels, result.pred_instances.scores, result.pred_instances.masks):
+            model_output = create_from_mask_rcnn_output(mask, bbox, label, score)
+            model_outputs.append((model_output, img))
 
     return model_outputs
