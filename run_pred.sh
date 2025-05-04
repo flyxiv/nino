c9b64749ed434fc829480db7f416185b0c130e99
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-05-04 15:10:23 +0900

    get argument

--- run_pred.sh
+++ run_pred.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python scripts/collect_sprites.py --model ./nino_seg_maskrcnn_e500.pth --config-file ./model/segmentation/mask_rcnn_resnet50/resnet_config.py --input-path ./data/coco/test2017/266cfc09-rpg39.png --model-type maskrcnn --output-dir ./output_data/sprites
+file_path=$1
+python scripts/collect_sprites.py --model ./trained_models/nino_seg_maskrcnn_e500.pth --config-file ./model/segmentation/mask_rcnn_resnet50/resnet_config.py --input-path $file_path --model-type maskrcnn --output-dir ./output_data/sprites
