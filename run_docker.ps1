d83229e01caad1160172516bce3d78c163682631
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-05-05 23:02:07 +0900

    fixed scripts

--- run_docker.ps1
+++ run_docker.ps1
@@ -2,7 +2,7 @@ $image_name = $args[0]
 
 # Docker 실행 명령
 docker run --gpus all -it `
-  -v ${PWD}/data/instance_segmentation_coco:/nino/data `
   -v ${PWD}/trained_models:/nino/trained_models `
   -v ${PWD}/output_data:/nino/output_data `
+  -v ${PWD}/input_files:/nino/input_files `
   "$image_name" bash
--- run_pred.sh
+++ run_pred.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-file_path=$1
-python scripts/collect_sprites.py --model ./trained_models/nino_seg_maskrcnn_e500.pth --config-file ./model/segmentation/mask_rcnn_resnet50/resnet_config.py --input-path $file_path --model-type maskrcnn --output-dir ./output_data/sprites
+python scripts/collect_sprites.py --model trained_models/nino_seg_maskcnn_e500.pth --config-file model/segmentation/mask_rcnn_resnet50/resnet_config.py --model-type maskrcnn --output-dir ./output_data --input-path ./input_files
